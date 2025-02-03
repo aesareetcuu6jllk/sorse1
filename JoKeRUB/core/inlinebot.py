2dcc6a544404e889822453565dc5160c49e07e83
aesaraesaraesar <aesar12345a@gmail.com>
2025-02-03 20:55:10 +0300

    Update inlinebot.py

--- JoKeRUB/core/inlinebot.py
+++ JoKeRUB/core/inlinebot.py
@@ -53,10 +53,10 @@ def ibuild_keyboard(buttons):
 
 
 def main_menu():
-    text = f"**- [سـورس robin ](https://t.me/RobinUserBot)\
+    text = f"**- [سـورس HELLAS ](https://t.me/n_nnae)\
         \n المسـاعد\
         \n\
-        \n◽ robin  لـ {mention}**"
+        \n◽ HELLAS  لـ {mention}**"
     buttons = [
         (Button.inline("ℹ️ • التعليمات •", data="check"),),
         (
@@ -228,7 +228,7 @@ async def inline_handler(event):  # sourcery no-metrics
             buttons = [
                 (
                     Button.inline("الحـالة ⚒️", data="stats"),
-                    Button.url(" JoKeRUB UsᴇʀBoᴛ", "https://t.me/RobinUserBot"),
+                    Button.url(" JoKeRUB UsᴇʀBoᴛ", "https://t.me/n_nnae"),
                 )
             ]
             ALIVE_PIC = gvarstatus("ALIVE_PIC")
@@ -336,7 +336,7 @@ async def inline_handler(event):  # sourcery no-metrics
 
             buttons = [Button.inline("عـرض الـرسـالة 🔐", data=f"troll_{timestamp}")]
             result = builder.article(
-                title="رسـالة تـرول من سـورس robin 🧸♥",
+                title="رسـالة تـرول من سـورس HELLAS  🧸♥",
                 text=f"فقـط {razan} هـو مـن يستـطيع مشـاهدتهـا !",
                 buttons=buttons,
             )
@@ -386,7 +386,7 @@ async def inline_handler(event):  # sourcery no-metrics
 
             buttons = [Button.inline("عـرض الـرسـالة 🔐", data=f"rzan_{timestamp}")]
             result = builder.article(
-                title="همسـة سـرية من سـورس robin 🧸♥",
+                title="همسـة سـرية من سـورس HELLAS  🧸♥",
                 text=f"فقـط {razan} هـو مـن يستـطيع مشـاهدتهـا !",
                 buttons=buttons,
             )
@@ -409,7 +409,7 @@ async def inline_handler(event):  # sourcery no-metrics
 
             buttons = [Button.inline("قراءة الـرسالـة ", data=f"hide_{timestamp}")]
             result = builder.article(
-                title="رسـالة مخفيـة من سـورس robin 🧸♥",
+                title="رسـالة مخفيـة من سـورس HELLAS  🧸♥",
                 text=f"✖✖✖",
                 buttons=buttons,
             )
@@ -559,10 +559,10 @@ async def inline_handler(event):  # sourcery no-metrics
     else:
         buttons = [
             (
-                Button.url("قنـاة السـورس ⚒️", "https://t.me/RobinUserBot"),
+                Button.url("قنـاة السـورس ⚒️", "https://t.me/n_nnae"),
                 Button.url(
                     "كـروب المـساعدة 📬",
-                    "https://t.me/uui7rb",
+                    "https://t.me/n_nnae1",
                 ),
             )
         ]
@@ -571,12 +571,12 @@ async def inline_handler(event):  # sourcery no-metrics
             url=ROZLOGO, size=0, mime_type="image/jpeg", attributes=[]
         )
         text, msg_entities = await event.client._parse_message_text(
-            "**[بـوت robin  🧸♥](https://t.me/RobinUserBot)**\
+            "**[بـوت HELLAS  🧸♥](https://t.me/n_nnae)**\
             \n\
-            \n❤ robin  هـو بـوت بسـيط يدخـل الـى حسـابك لجعـلك تتحـكم به معـ اوامـر معينـة.\
+            \n❤ HELLAS  هـو بـوت بسـيط يدخـل الـى حسـابك لجعـلك تتحـكم به معـ اوامـر معينـة.\
             \n\
             \n**اذا كـنت تـرغب بتـنصيـب السـورس عـلى حسـابك ,\
-            \n🐾 تعال الـى [كروب المسـاعدة](https://t.me/uui7rb)!**",
+            \n🐾 تعال الـى [كروب المسـاعدة](https://t.me/n_nnae1)!**",
             "md",
         )
         result = types.InputBotInlineResult(
@@ -584,7 +584,7 @@ async def inline_handler(event):  # sourcery no-metrics
             type="photo",
             title="l313l 🧸♥",
             description="ادخـل كـروب المسـاعدة",
-            url="https://t.me/uui7rb",
+            url="https://t.me/n_nnae1",
             thumb=photo,
             content=photo,
             send_message=types.InputBotInlineMessageMediaAuto(
