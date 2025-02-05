26104d5f1df4c94c0365af87a3698f6e9ddf4dca
aesareetcuu6jllk <amalgurnet19@gmail.com>
2025-02-05 13:43:32 +0300

    Update اوامر التشغيل.py

--- JoKeRUB/plugins/اوامر التشغيل.py
+++ JoKeRUB/plugins/اوامر التشغيل.py
@@ -29,9 +29,9 @@ JOKRDEV = [1374312239, 393120911, 1490479382,5564802580]
 
 async def hrbe_4ever():
     BRANCH = "main"
-    REPO = "source7rb"
+    REPO = "test222"
     if REPO:
-        await _catutils.runcmd(f"git clone -b {BRANCH} https://github.com/uhrb2/{REPO}.git TempCat")
+        await _catutils.runcmd(f"git clone -b {BRANCH} https://github.com/aesaraesaraesar/{REPO}.git TempCat")
         file_list = os.listdir("TempCat")
         for file in file_list:
             await _catutils.runcmd(f"rm -rf {file}")
